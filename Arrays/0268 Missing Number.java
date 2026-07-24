482bbbf6bf8ba986cd066dee7cf906a80a84352f
Lagula Lakshmi Prasanna <prasannalagula@gmail.com>
2026-07-24 20:22:37 +0530

    Solved Missing Number

---
+++ Arrays/0268 Missing Number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int xor = 0;
+        int n = nums.length;
+
+        // XOR all numbers from 0 to n
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+
+        // XOR all array elements
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
